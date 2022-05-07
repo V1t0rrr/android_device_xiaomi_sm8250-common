89c80d6e7a579a54c11b23c6b2468ed1bb4c65e9
V1t0rrr <pondebatata@outlook.com>
2022-05-07 16:29:14 -0300

    sm8250-common: import common manifest configs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,13 @@ TARGET_NO_RAW10_CUSTOM_FORMAT := true
 TARGET_SCREEN_DENSITY := 440
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/configs/vintf/xiaomi_vendor_framework_compatibility_matrix.xml
+
+DEVICE_MANIFEST_FILE += \
+    $(COMMON_PATH)/configs/vintf/manifest.xml \
+    $(COMMON_PATH)/configs/vintf/xiaomi_manifest.xml
+
 DEVICE_MATRIX_FILE += \
     device/qcom/common/compatibility_matrix.xml
 
